3dfe61f9760035b94aeea3154e020c0fb68b2a21
jenz0000 <2025.jenz@gmail.com>
2023-03-02 22:06:16 +0900

    ✨ Add BaseModel class

---
+++ config/base/models.py
@@ -0,0 +1,16 @@
+# django
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def change(self, **kwargs):
+        for field, value in kwargs.items():
+            setattr(self, field, value)
+
+        self.save(update_fields=kwargs)
